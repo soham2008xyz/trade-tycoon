870425b7315b078ab22954f22c9d0276fce0b2c1
Soham Banerjee <soham@renderbit.com>
2026-01-08 21:41:13 +0530

    chore: remove unused boilerplate code and components

--- apps/client/components/Board.tsx
+++ apps/client/components/Board.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { View, StyleSheet, Dimensions, useWindowDimensions, Text } from 'react-native';
+import { View, StyleSheet, useWindowDimensions } from 'react-native';
 import { BOARD, Player } from '@trade-tycoon/game-logic';
 import { Tile } from './Tile';
 
